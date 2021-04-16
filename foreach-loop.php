288810ec9cf190617f8f4d86739a5d6388fa4bee
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2021-04-16 13:58:05 +0600

    Update foreach-loop.php

--- foreach-loop.php
+++ foreach-loop.php
@@ -1,6 +1,81 @@
 <?php
-  $a = [ 'A', 'B', 'C', 'D' ];
+  // Index Array or Numeric Array
+  $index = [ 'A', 'B', 'C', 'D' ];
   
-  foreach( $a as $key => $value ){
+  foreach( $index as $key => $value ){
     echo $key . ' = ' . $value . '<br>';
   }
+
+ // Associative Array
+ $assoc = [
+    'a' => 'A',
+    'b' => 'B'
+    'c' => 'C',
+    'd' => 'D',
+ ];
+  
+  foreach( $assoc as $key => $value ){
+    echo $key . ' = ' . $value . '<br>';
+  }
+
+  // Multidimensional Array
+  $multi = [
+    [ 
+      'a' => 'A',
+      'b' => 'B'
+      'c' => 'C',
+      'd' => 'D',
+    ],
+    [ 
+      'e' => 'E',
+      'f' => 'F'
+      'g' => 'G',
+      'h' => 'H',
+    ],
+  ];
+  
+  foreach( $multi as $key => $value ){
+    foreach( $value as $key2 => $value2 ){
+      echo $key2 . ' = ' . $value2 . '<br>';
+    }
+  }
+
+
+  // Multidimensional Associative Array
+    $multi_assoc = [
+      'emon' => [ 
+        'a' => 'A',
+        'b' => 'B'
+        'c' => 'C',
+        'd' => 'D',
+      ],
+      'fatema' => [ 
+        'e' => 'E',
+        'f' => 'F'
+        'g' => 'G',
+        'h' => 'H',
+      ],
+    ];
+
+    foreach( $multi as $key => $value ){
+      foreach( $value as $key2 => $value2 ){
+        echo $key2 . ' = ' . $value2 . '<br>';
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
