afec6adc24f7a86ff5bc43c30e76b313ece28a15
smemonislam6 <smemonislam6@gmail.com>
2021-04-16 15:30:02 +0600

    I have add multidiemensional associative array advanced part

--- foreach-loop.php
+++ foreach-loop.php
@@ -1,69 +1,109 @@
 <?php
-  // Index Array or Numeric Array
-  $index = [ 'A', 'B', 'C', 'D' ];
-  
-  foreach( $index as $key => $value ){
-    echo $key . ' = ' . $value . '<br>';
-  }
+// Index Array or Numeric Array
+$index = [ 'A', 'B', 'C', 'D' ];
 
- // Associative Array
- $assoc = [
+foreach( $index as $key => $value ){
+  echo $key . ' = ' . $value . '<br>';
+}
+
+// Associative Array
+$assoc = [
+  'a' => 'A',
+  'b' => 'B',
+  'c' => 'C',
+  'd' => 'D',
+];
+
+foreach( $assoc as $key => $value ){
+  echo $key . ' = ' . $value . '<br>';
+}
+
+// Multidimensional Array
+$multi = [
+  [ 
     'a' => 'A',
     'b' => 'B',
     'c' => 'C',
     'd' => 'D',
- ];
-  
-  foreach( $assoc as $key => $value ){
-    echo $key . ' = ' . $value . '<br>';
-  }
+  ],
+  [ 
+    'e' => 'E',
+    'f' => 'F',
+    'g' => 'G',
+    'h' => 'H',
+  ],
+];
 
-  // Multidimensional Array
-  $multi = [
-    [ 
+foreach( $multi as $key => $value ){
+  foreach( $value as $key2 => $value2 ){
+    echo $key2 . ' = ' . $value2 . '<br>';
+  }
+}
+
+
+// Multidimensional Associative Array
+  $multi_assoc = [
+    'emon' => [ 
       'a' => 'A',
       'b' => 'B',
       'c' => 'C',
       'd' => 'D',
     ],
-    [ 
+    'fatema' => [ 
       'e' => 'E',
       'f' => 'F',
       'g' => 'G',
       'h' => 'H',
     ],
   ];
-  
+
   foreach( $multi as $key => $value ){
     foreach( $value as $key2 => $value2 ){
       echo $key2 . ' = ' . $value2 . '<br>';
     }
   }
 
+$multi_assoc_ad = [
+  'emon' => [ 
+    'a' => 'A',
+    'b' => 'B',
+    'c' => 'C',
+    'd' => 'D',
+    'emo' => [ 
+      'n' => 'N',
+      'o' => 'O',
+      'p' => 'P'
+    ]
+  ],
+  'fatema' => [
+    'e' => 'E',
+    'f' => 'F',
+    'g' => 'G',
+    'abeda' => [
+      'h' => 'H',
+      'i' => 'I',
+      'j' => 'J',
+    ],
+    'suchi' => [
+      'k' => 'K',
+      'l' => 'L',
+      'm' => 'M',
+    ],
+  ]
+];
 
-  // Multidimensional Associative Array
-    $multi_assoc = [
-      'emon' => [ 
-        'a' => 'A',
-        'b' => 'B',
-        'c' => 'C',
-        'd' => 'D',
-      ],
-      'fatema' => [ 
-        'e' => 'E',
-        'f' => 'F',
-        'g' => 'G',
-        'h' => 'H',
-      ],
-    ];
-
-    foreach( $multi as $key => $value ){
-      foreach( $value as $key2 => $value2 ){
-        echo $key2 . ' = ' . $value2 . '<br>';
+foreach( $multi_assoc_ad as $key => $value ){
+  foreach( $value as $key2 => $value2 ){
+    if( is_array( $value2 ) ){
+      foreach( $value2 as $key3 => $value3){
+        echo $key3 . ' = ' . $value3 . '<br>';
       }
     }
-
-
+    else {
+      echo $key2 . ' = ' . $value2 . '<br>';
+    }
+  }
+}
 
 
 
